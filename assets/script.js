60c6487dc7b6cca303fee0f9c959bdb14cd42275
Gabe <gabriel.velasquez0116@gmail.com>
2023-07-21 13:49:43 -0700

    function added for country search

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,14 @@ let apiUrl = 'http://api.accuweather.com/locations/v1/search?q=san&apikey=';
 
 // RESTcountry API fetch
 function citySearch(){
-
-}
+    fetch(`ttps://restcountries.com/v3.1/name/${countrySearch}`)
+        .then(function(response){
+            return response.json();
+        })
+        .catch(function(error){
+            //Modal
+        });
+};
 
 // Search function and fetches API request 
 function weatherSearch(){
