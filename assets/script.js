f37cc3c861f6e9fea207214936b2171360b99025
Gabe Velasquez <55639852+Gabe-Velasquez@users.noreply.github.com>
2023-07-21 20:57:40 -0700

    Javascript (#3)
    * #2 added weather api information and provided fetch request in function
    
    * adding more variables to script js
    
    * function added for country search
    
    * edit on functions to be able to use json response

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+// Selectors to link html elements
+let submitBtn = document.querySelector('');
+let countrySearch = document.getElementById('#');
+let citySelect = document.getElementById('#');
+
+let cityResult = [];
+
+// Accuweather API 
+const apiKey = 'j3dU13wPqsC6XNhy4fabRe4Rta1qbIKp';
+let apiUrl = 'http://api.accuweather.com/locations/v1/search?q=san&apikey=';
+
+
+// RESTcountry API fetch
+function citySearch(){
+    fetch(`ttps://restcountries.com/v3.1/name/${countrySearch}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+
+        })
+        .catch(function(error){
+            //Modal
+        });
+};
+
+// Search function and fetches API request 
+function weatherSearch(){
+   fetch(apiUrl + apiKey)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            
+        })
+        .catch(function(error){
+            //modal alert goes here
+        });
+};
