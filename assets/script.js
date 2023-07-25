99991c9cbdb7be6885eb4b4e78301ca0657b087e
strunk <astrunk87@gmail.com>
2023-07-24 21:44:49 -0500

    added event lister to countries that changes cities

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 // Selectors to link html elements
-let submitBtn = document.querySelector('');
+// let submitBtn = document.querySelector('#');
 let countrySearch = document.getElementById('#');
 let citySelect = document.getElementById('#');
+var countryUS = document.getElementById("us");
+var cityOption1 = document.getElementById("city1");
+var cityOption2 = document.getElementById("city2");
+var cityOption3 = document.getElementById("city3");
+var cityOption4 = document.getElementById("city4");
+
 
 let cityResult = [];
 
@@ -37,3 +43,25 @@ function weatherSearch(){
             //modal alert goes here
         });
 };
+
+// drop down countries
+// if function so when a country is picked the cities option changes in the next box
+countryUS.addEventListener("click", function(event) {
+    cityOption1.textContent = "LA";
+    cityOption2.textContent = "NY";
+    cityOption3.textContent = "MPLS";
+    cityOption4.textContent = "LV";
+
+  });
+
+// usCities:
+//   losAngels
+//   newYork
+//   minneapolis
+
+// germanyCities:
+
+
+
+
+
