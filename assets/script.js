a1b4e8119b4c9f977ba6c06c1ab9f250234bdf6a
strunk <astrunk87@gmail.com>
2023-07-26 13:05:38 -0500

    added cities to drop down for country 1

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,10 @@
 let countrySearch = document.getElementById('#');
 let citySelect = document.getElementById('#');
 var countryUS = document.getElementById("us");
+var countryMX = document.getElementById("mx");
+var countryDE = document.getElementById("de");
+var countryKR = document.getElementById("kr");
+var countryAU = document.getElementById("au");
 var cityOption1 = document.getElementById("city1");
 var cityOption2 = document.getElementById("city2");
 var cityOption3 = document.getElementById("city3");
@@ -53,6 +57,38 @@ countryUS.addEventListener("click", function(event) {
     cityOption4.textContent = "LV";
 
   });
+
+  countryMX.addEventListener("click", function(event) {
+    cityOption1.textContent = "Mexico City";
+    cityOption2.textContent = "Cancun";
+    cityOption3.textContent = "Guadalajara";
+    cityOption4.textContent = "Tijuana";
+
+  });  
+
+  countryDE.addEventListener("click", function(event) {
+    cityOption1.textContent = "Berlin";
+    cityOption2.textContent = "Munich";
+    cityOption3.textContent = "Frankfurt";
+    cityOption4.textContent = "Hamburg";
+
+  });
+
+  countryKR.addEventListener("click", function(event) {
+    cityOption1.textContent = "Seoul";
+    cityOption2.textContent = "Busan";
+    cityOption3.textContent = "Gwangju";
+    cityOption4.textContent = "Incheon";
+
+  });  
+
+  countryAU.addEventListener("click", function(event) {
+    cityOption1.textContent = "Sydney";
+    cityOption2.textContent = "Melbourne";
+    cityOption3.textContent = "Cairns";
+    cityOption4.textContent = "Adelaide";
+
+  });  
 
 // usCities:
 //   losAngels
